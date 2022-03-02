0f92c07f94ccad8fd2c7345c5e154bea66b06cd5
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2022-03-02 00:21:46 -0600

    protect api

--- pages/api/user/update-coin.ts
+++ pages/api/user/update-coin.ts
@@ -1,22 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../lib/prisma";
+import { getSession } from "next-auth/react";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { email } = req.body;
-  const user = await prisma.user.update({
-    where: {
-      email: email,
-    },
-    data: {
-      coins: {
-        increment: 1,
+  const session = await getSession({ req });
+  if (session) {
+    const { email } = req.body;
+    const user = await prisma.user.update({
+      where: {
+        email: email,
       },
-    },
-  });
+      data: {
+        coins: {
+          increment: 1,
+        },
+      },
+    });
 
-  res.status(200).send(user);
+    res.status(200).send(user);
+  } else {
+    res.status(401);
+  }
 }
